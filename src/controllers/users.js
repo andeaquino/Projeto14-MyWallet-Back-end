346c8f6a11a71cb348fa1a9fbbea4329c17dbc7c
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-10-21 10:34:00 -0300

    created route to sign-up users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,10 +1,17 @@
 import connection from "../database/database.js";
+import bcrypt from 'bcrypt';
 
 const postUsers = async (req, res) => {
     const {name, email, password} = req.body;
 
+    const passwordHash = bcrypt.hashSync(password, 10);
+
     try {
-        await connection.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, password]);
+        await connection.query(`
+            INSERT INTO users 
+            (name, email, password) 
+            VALUES ($1, $2, $3)
+        `, [name, email, passwordHash]);
 
         res.sendStatus(201);
     } catch {
