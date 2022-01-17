79ea94d08a37f4688cba6f7ba210055a91439dc4
Anderson Aquino <andersonmr.aquino@gmail.com>
2022-01-17 10:39:27 -0300

    created user service and controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 import * as userService from "../services/userService";
+import { signUpSchema, signInSchema } from "../schemas/usersSchemas";
 
 export async function getUsers (req: Request, res: Response) {
   try {
@@ -11,3 +12,43 @@ export async function getUsers (req: Request, res: Response) {
     res.sendStatus(500);
   }
 }
+
+async function signUp(req: Request, res: Response) {
+  try {
+    const isBodyInvalid = signUpSchema.validate(req.body).error
+    if (isBodyInvalid) return res.sendStatus(403);
+
+    const user = await userService.registerUser(req.body);
+
+    if (!user) {
+      return res.sendStatus(409);
+    }
+
+    return res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+}
+
+async function signIn(req: Request, res: Response) {
+  try {
+    const isBodyInvalid = signInSchema.validate(req.body).error
+    if (isBodyInvalid) return res.sendStatus(403);
+
+    const user = await userService.authenticate(req.body);
+
+    if (user) {
+      return res.send({
+        token: user.token,
+        name: req.body.name,
+      });
+    }
+    return res.sendStatus(401);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+}
+
+export { signUp, signIn };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 import User from "../entities/User";
 
@@ -10,3 +12,30 @@ export async function getUsers () {
   
   return users;
 }
+
+async function registerUser(user: User) {
+  const existingUser = await getRepository(User).find({ email: user.email });
+
+  if (existingUser.length !== 0) return false;
+
+  user.password = bcrypt.hashSync(user.password, 12);
+  await getRepository(User).insert(user);
+
+  return true;
+}
+
+async function authenticate(user: User) {
+  const existingUser = await getRepository(User).findOne({ email: user.email });
+
+  if (!existingUser || !bcrypt.compareSync(existingUser.password, user.password)) {
+    return false;
+  }
+
+  const token = jwt.sign({
+    userId: existingUser.id,
+  }, process.env.JWT_SECRET);
+
+  return { token };
+}
+
+export { registerUser, authenticate };
