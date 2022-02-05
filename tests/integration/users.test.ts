9e045aed185730aecc5066b52b88a707f903b0c6
Anderson Aquino <andersonmr.aquino@gmail.com>
2022-02-04 22:54:10 -0300

    fix: test routes and responses

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -1,5 +1,6 @@
 import supertest from "supertest";
 import { getConnection } from "typeorm";
+import http from "../../src/enums/http.status";
 
 import app, { init } from "../../src/app";
 import { createUser, generateUserBody } from "../factories/userFactory";
@@ -18,56 +19,56 @@ afterAll(async () => {
 });
 
 describe("POST /sign-up", () => {
-  it("should return status 403 for invalid body", async () => {
+  it("should return unprocessable entity for invalid body", async () => {
     const body = {};
 
     const result = await supertest(app).post("/sign-up").send(body);
     const { status } = result;
-    expect(status).toEqual(403);
+    expect(status).toEqual(http.UNPROCESSABLE_ENTITY);
   });
 
-  it("should return status 201 when account is created sucessufully", async () => {
+  it("should return status created when account is created sucessufully", async () => {
     const body = generateUserBody();
 
     const result = await supertest(app).post("/sign-up").send(body);
     const { status } = result;
-    expect(status).toEqual(201);
+    expect(status).toEqual(http.CREATED);
   });
 
-  it("should return status 409 when email is already in use", async () => {
+  it("should return conflict when email is already in use", async () => {
     const user = await createUser();
     const body = generateUserBody({ email: user.email });
 
     const result = await supertest(app).post("/sign-up").send(body);
     const { status } = result;
-    expect(status).toEqual(409);
+    expect(status).toEqual(http.CONFLICT);
   });
 });
 
 describe("POST /sign-in", () => {
-  it("should return status 403 for invalid body", async () => {
+  it("should return unprocessable entity for invalid body", async () => {
     const body = {};
 
     const result = await supertest(app).post("/sign-in").send(body);
     const status = result.status;
-    expect(status).toEqual(403);
+    expect(status).toEqual(http.UNPROCESSABLE_ENTITY);
   });
 
-  it("should return status 200 when account is logged in sucessufully", async () => {
+  it("should return status OK when account is logged in sucessufully", async () => {
     const user = await createUser();
     const body = { email: user.email, password: user.password };
 
     const result = await supertest(app).post("/sign-in").send(body);
     const status = result.status;
-    expect(status).toEqual(200);
+    expect(status).toEqual(http.OK);
   });
 
-  it("should return status 401 when email or password does not match or exist", async () => {
+  it("should return unauthorized when email or password does not match or exist", async () => {
     const user = generateUserBody();
     const body = { email: user.email, password: user.password };
 
     const result = await supertest(app).post("/sign-in").send(body);
     const status = result.status;
-    expect(status).toEqual(401);
+    expect(status).toEqual(http.UNAUTHORIZED);
   });
 });
